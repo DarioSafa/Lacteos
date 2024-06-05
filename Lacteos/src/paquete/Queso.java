427df5cf6082ecf1a2761916ea399806dd7e508b
Dario Martin <d.martin1@fp.safavalladolid.com>
2024-06-05 12:53:00 +0200

    Creada la clase queso y agregados sus atributos

---
+++ Lacteos/src/paquete/Queso.java
@@ -0,0 +1,11 @@
+package paquete;
+
+public class Queso {
+	/* Indica el tipo de leche con el que se ha fabricado el queso*/
+	String tipo_leche;
+	/* Indica la cantidad en milimetros utilizada para la fabricacion del queso*/
+	int cantidad_leche;
+	/* Indica el peso en gramos del queso*/
+	double peso;
+	
+}
